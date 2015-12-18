05567943b52dad67090c2b4d04f7f4d46efd0bc1
Gabriel <Gabriel@DESKTOP-BBHCPU9>
2015-12-18 19:12:34 -0300

    el gabi comitajeando

--- src/ventanas/GestionarResultados.java
+++ src/ventanas/GestionarResultados.java
@@ -268,6 +268,8 @@ public class GestionarResultados extends javax.swing.JPanel {
                 JOptionPane.showMessageDialog(null, "Resultado guardado exitosamente", "", JOptionPane.INFORMATION_MESSAGE);
     }//GEN-LAST:event_jButton1ActionPerformed
 
+            
+        }}
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         V.get().remove(this);
         V.get().gestionarResultadosVolver();
