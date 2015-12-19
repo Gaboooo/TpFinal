cd39530cc1b9e79e362a039de970fea5ee3be3ea
Martin <Martin@192.168.0.10>
2015-12-19 00:42:14 -0300

    Figue

--- src/gestor/GenerarFixtureGestor.java
+++ src/gestor/GenerarFixtureGestor.java
@@ -84,7 +84,6 @@ public class GenerarFixtureGestor {
                 
                 for(int j=0; j<cantPartidosPorRonda; j++){
                     
-                    
                     // Saca un par de la lista, dentro se verifica que los participantes no esten usados,
                     // y que se usen los participantes que se dejaron afuera en las rondas anteriores
                     parAux=getPar(listaPares, listaUsados, listaDejadosAfuera);
@@ -130,51 +129,6 @@ public class GenerarFixtureGestor {
         }
     }
     
-    // Saca un par de P1 vs P2 en el que se encuentra el participante pedido
-    private static ArrayList<Participante> getParObligatorio(ArrayList<ArrayList<Participante>> listaPares,
-            Participante participante, ArrayList<Participante> listaDejadosAfuera){
-        
-        ArrayList<Participante> par= null;
-        ArrayList<Participante> parAux= null;
-        
-        // Hay participantes que ya se han dejado afuera en rondas anteriores
-        // Entonces hay que darles prioridades a estos
-        if (listaDejadosAfuera.size()>1){
-            for(int i=0; i<listaPares.size(); i++){
-                parAux=listaPares.get(i);
-                if(listaDejadosAfuera.contains(parAux.get(0)) && listaDejadosAfuera.contains(parAux.get(1))
-                        && (parAux.get(0)==participante || parAux.get(1)==participante)){
-                    par=parAux;
-                    listaPares.remove(parAux);
-                    break;
-                }
-            }
-        }
-        if (listaDejadosAfuera.size()>1 && par==null){
-            for(int i=0; i<listaPares.size(); i++){
-                parAux=listaPares.get(i);
-                if((listaDejadosAfuera.contains(parAux.get(0)) || listaDejadosAfuera.contains(parAux.get(1)))
-                        && (parAux.get(0)==participante || parAux.get(1)==participante)){
-                    par=parAux;
-                    listaPares.remove(parAux);
-                    break;
-                }
-            }
-        }
-        if(parAux==null){
-            for(int i=0; i<listaPares.size(); i++){
-                parAux=listaPares.get(i);
-                // Se encuentra en la primera o segunda posicion
-                if(parAux.get(0)==participante || parAux.get(1)==participante){
-                    par=parAux;
-                    listaPares.remove(parAux);
-                    break;
-                }
-            }
-        }
-        return par;
-    }
-    
     // Saca un par fijandose que no este utilizado
     private static ArrayList<Participante> getPar(ArrayList<ArrayList<Participante>> listaPares,
             ArrayList<Participante> listaUsados, ArrayList<Participante> listaDejadosAfuera){
@@ -218,6 +172,9 @@ public class GenerarFixtureGestor {
                     i+=listaPares.size();
                 }
             }
+        }
+        if (par==null){
+            par=null;
         }
         return par;
     }
